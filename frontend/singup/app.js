55ef55cfc48bb9b3d47a576af239f26d58fe4b0e
amiram505013 <amiram5050.13@gmail.com>
2023-10-02 18:45:41 +0330

    name

---
+++ frontend/singup/app.js
@@ -0,0 +1,39 @@
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const submit = document.getElementById("submit");
+const resultElement = document.getElementById("result");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    input1.addEventListener("input", compareInputs);
+    input2.addEventListener("input", compareInputs);
+
+    function compareInputs() {
+        const value1 = input1.value.trim();
+        const value2 = input2.value.trim();
+
+        if (value1.length >= 8 && value2.length >= 8 && /^[a-zA-Z0-9]+$/.test(value1) && /^[a-zA-Z0-9]+$/.test(value2)) {
+            if (value1 === value2) {
+                input1.style.borderBottomColor = "green";
+                input1.style.color = "green";
+                input2.style.borderBottomColor = "green";
+                input2.style.color = "green";
+                submit.removeAttribute("disabled"); // فعال کردن دکمه ارسال
+            } else {
+                input1.style.borderBottomColor = "red";
+                input1.style.color = "red";
+                input2.style.borderBottomColor = "red";
+                input2.style.color = "red";
+                submit.setAttribute("disabled", "disabled"); // غیرفعال کردن دکمه ارسال
+            }
+        } else {
+            resultElement.textContent = "حداقل 8 رقم و از حروف و اعداد استفاده کنیAt least 8 digits and use letters and numbers";
+            resultElement.style.color = 'red';
+            input1.style.borderBottomColor = "red";
+            input1.style.color = "red";
+            input2.style.borderBottomColor = "red";
+            input2.style.color = "red";
+            submit.setAttribute("disabled", "disabled"); // غیرفعال کردن دکمه ارسال
+        }
+    }
+});
